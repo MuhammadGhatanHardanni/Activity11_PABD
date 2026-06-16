da54e1101d9f7059881dfb51bd950d93ee02534a
Ghatan55 <ghatanhardanni@gmail.com>
2026-06-16 13:03:47 +0700

    Add project files.

---
+++ CRUDMahasiswaADO/Form2.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form2 : Form
+    {
+        // Deklarasi variabel untuk koneksi dan manipulasi data dari database
+        static string connectionString = "Data Source=LAPTOP-VL5SDNPR\\GHATANHARDANNI;Initial Catalog=DB_AkademikADO; User ID=sa;Password=12345678";
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // Mengatur format kalender menjadi hanya Tahun
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            // Mencegah user mengetik manual di combobox
+            cmbProdi.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Mematikan tombol cetak sebelum data di-load
+            btnCetak.Enabled = false;
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                // Mengambil data nama prodi untuk dimasukkan ke combobox
+                SqlCommand cmd = new SqlCommand("select namaprodi from programstudi", conn);
+                cmd.CommandType = CommandType.Text;
+
+                dtProdi = new DataTable();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(dtProdi);
+
+                cmbProdi.DataSource = dtProdi;
+                cmbProdi.DisplayMember = "namaprodi";
+                cmbProdi.ValueMember = "namaprodi";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal Load data: " + ex.Message);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // Memasukkan parameter dari UI (Combobox dan DateTimePicker) ke Stored Procedure
+                cmd.Parameters.Add("@inProdi", SqlDbType.VarChar, 50).Value = cmbProdi.SelectedValue;
+                cmd.Parameters.Add("@inTgLMsuk", SqlDbType.VarChar, 4).Value = dtpTanggalMasuk.Value.Year.ToString();
+
+                da = new SqlDataAdapter(cmd);
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                // Menampilkan hasil dari DataTable ke DataGridView
+                dataGridView1.DataSource = dtMahasiswa;
+
+                // Logika mengaktifkan tombol cetak jika data ada (lebih dari 0 baris)
+                if (dtMahasiswa.Rows.Count > 0)
+                {
+                    btnCetak.Enabled = true;
+                }
+                else
+                {
+                    btnCetak.Enabled = false;
+                    MessageBox.Show("Data tidak ditemukan");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal Load data: " + ex.Message);
+            }
+        }
+
+        private void btnCetak_Click(object sender, EventArgs e) {
+            try
+            {
+                // Memanggil FormReport dan mengirim parameter nama Prodi dan Tahun
+                FormReport frmReport = new FormReport(cmbProdi.Text, dtpTanggalMasuk.Value);
+                frmReport.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal membuka laporan: " + ex.Message);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void dtpTanggalMasuk_ValueChanged(object sender, EventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
+        private void cmbProdi_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+    }
+}
---
+++ CRUDMahasiswaADO/FormReport.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient; // Tambahan wajib untuk SQL Server
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class FormReport : Form
+    {
+        // Deklarasi variabel SQL
+        static string connectionString = "Data Source=LAPTOP-VL5SDNPR\\GHATANHARDANNI;Initial Catalog=DB_AkademikADO; User ID=sa;Password=12345678";
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+
+        // Properti penampung parameter filter
+        string prodi { get; set; }
+        DateTime tglmasuk { get; set; }
+
+        // Ubah Constructor agar menerima parameter Prodi dan Tanggal
+        public FormReport(string Prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+
+            prodi = Prodi;
+            tglmasuk = TglMasuk;
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                // Mengeksekusi Stored Procedure
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // Menambahkan parameter sesuai tipe data di prosedur
+                cmd.Parameters.AddWithValue("@inProdi", prodi);
+                cmd.Parameters.AddWithValue("@inTglMsuk", tglmasuk.Year.ToString()); // Ambil tahunnya saja
+
+                da = new SqlDataAdapter(cmd);
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+                conn.Close();
+
+                // Instansiasi file desain .rpt Anda
+                // Pastikan "ReportMahasiswa" sesuai dengan nama file .rpt yang Anda buat
+                ReportMahasiswa report = new ReportMahasiswa();
+                report.SetDataSource(dtMahasiswa);
+
+                // Tampilkan ke CrystalReportViewer
+                crystalReportViewer1.ReportSource = report;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal Load data: " + ex.Message);
+            }
+        }
+    }
+}
