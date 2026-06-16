1b72c658519dd770287ce6e021a5f729edebc146
Ghatan55 <ghatanhardanni@gmail.com>
2026-06-16 13:06:42 +0700

    Membuat Koneksi Ke database pada Class DAL

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient; // Wajib ditambahkan untuk koneksi SQL Server
+
+namespace CRUDMahasiswaADO
+{
+    internal class DAL
+    {
+        // Connection string sudah disesuaikan dengan server kamu
+        static string connectionString = "Data Source=LAPTOP-VL5SDNPR\\GHATANHARDANNI; Initial Catalog=DB_AkademikADO; User ID=sa; Password=12345678;";
+
+        public string GetConnectionString()
+        {
+            return connectionString;
+        }
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+        
+    }
+}
